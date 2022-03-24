23f08fca48abe5d6afc0067ca0cb214101161064
Samuel Medina <smdoomed@gmail.com>
2022-03-24 13:58:39 -0600

    nuevo repo :v

---
+++ index.js
@@ -0,0 +1,8 @@
+const header = document.querySelector(".header");
+window.addEventListener("scroll", function(e) {
+    if (window.scrollY >  80) {
+        header.classList = "header bg";
+    } else {
+        header.classList = "header";
+    }
+});
